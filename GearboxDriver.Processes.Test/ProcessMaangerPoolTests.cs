04e0c07a43ccbd254fff43f2448db5a4a34baa39
Maciej Białobrzeski <bialobrzeskimaciek@gmail.com>
2020-05-10 17:52:32 +0200

    Add GearboxDriver.Processes Tests

---
+++ GearboxDriver.Processes.Test/ProcessMaangerPoolTests.cs
@@ -0,0 +1,42 @@
+﻿using GearboxDriver.Seedwork;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GearboxDriver.Processes.Test
+{
+    public class ProcessMaangerPoolTests
+    {
+        [Test]
+        public void CannotAddSameProcessManagersTwice()
+        {
+            var processManagerPool = new ProcessManagerPool();
+
+            var processManager = new Mock<IProcessManager>();
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                processManagerPool.Add(processManager.Object);
+                processManagerPool.Add(processManager.Object);
+            });
+        }
+
+        [Test]
+        public void CannotRemoveSameProcessManagerTwice()
+        {
+            var processManagerPool = new ProcessManagerPool();
+
+            var processManager = new SmoothBrakingWithTrailerAttached();
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                processManagerPool.Add(processManager);
+                processManagerPool.Remove(typeof(SmoothBrakingWithTrailerAttached));
+                processManagerPool.Remove(typeof(SmoothBrakingWithTrailerAttached));
+            });
+        }
+    }
+}
---
+++ GearboxDriver.Processes.Test/SmoothBrakingWithTrailerAttachedTests.cs
@@ -0,0 +1,26 @@
+﻿using GearboxDriver.Hardware.ACL;
+using GearboxDriver.Seedwork;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GearboxDriver.Processes.Test
+{
+    public class SmoothBrakingWithTrailerAttachedTests
+    {
+        [Test]
+        public void WhenVehicleTiltedDownHillCarIsMovingDownhill()
+        {
+            var manager = new SmoothBrakingWithTrailerAttached();
+
+            var @event = new VehicleTiltedDownhill();
+
+            manager.ApplyEvent(@event);
+
+            
+        }
+
+    }
+}

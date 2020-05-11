7bbfc5a56ed5c8fbf0e5f9ba7c91ff9288be5e11
Maciej Białobrzeski <bialobrzeskimaciek@gmail.com>
2020-05-11 22:52:15 +0200

    Change RpmBasedByModes

---
+++ GearboxDriver.Processes/Characteristics.cs
@@ -0,0 +1,32 @@
+﻿using GearboxDriver.Cabin.Responsiveness;
+using GearboxDriver.Gearshift;
+using GearboxDriver.Hardware.ACL;
+using System.Collections.Generic;
+
+namespace GearboxDriver.Processes
+{
+    public class Characteristics
+    {
+        private readonly Dictionary<ResponsivenessMode, ShiftpointRange> RangeForModes =
+            new Dictionary<ResponsivenessMode, ShiftpointRange>
+            {
+                { ResponsivenessMode.Economic, new ShiftpointRange(new Rpm(1000d), new Rpm(2000d)) },
+                { ResponsivenessMode.Comfort, new ShiftpointRange(new Rpm(1000d), new Rpm(2500d)) },
+                { ResponsivenessMode.Sport, new ShiftpointRange(new Rpm(1500d), new Rpm(5000d)) }
+            };
+
+        private readonly Dictionary<AggressivenessLevel, Percentage> PercentageForAggressivenesLevel =
+            new Dictionary<AggressivenessLevel, Percentage>
+            {
+                { AggressivenessLevel.First, new Percentage(1.0d) },
+                { AggressivenessLevel.Second, new Percentage(1.3d) },
+                { AggressivenessLevel.Third, new Percentage(1.3d) }
+            };
+
+        public ShiftpointRange GetRangeForResponsivenessMode(ResponsivenessMode responsivenessMode)
+            => RangeForModes[responsivenessMode];
+
+        public Percentage GetPercentageForAggressivenessLevel(AggressivenessLevel aggressivenessLevel)
+            => PercentageForAggressivenesLevel[aggressivenessLevel];
+    }
+}
--- GearboxDriver.Processes/RpmBasedByModes.cs
+++ GearboxDriver.Processes/RpmBasedByModes.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using GearboxDriver.Cabin.Responsiveness;
+﻿using GearboxDriver.Cabin.Responsiveness;
 using GearboxDriver.Gearshift;
-using GearboxDriver.Hardware.ACL;
 using GearboxDriver.Seedwork;
 
 namespace GearboxDriver.Processes
@@ -9,23 +7,14 @@ namespace GearboxDriver.Processes
     public class RpmBasedByModes : IProcessManager
     {
         private readonly IGearshiftService _service;
-        private ResponsivenessMode ResponsivenessMode { get; set; }
-        private AggressivenessLevel AggressivenessLevel { get; set; }
+        private readonly Characteristics _characteristics;
+        private ResponsivenessMode CurrentResponsivenessMode { get; set; }
+        private AggressivenessLevel CurrentAggressivenessLevel { get; set; }
 
-        private readonly Dictionary<ResponsivenessMode, ShiftpointRange> RangeForModes = new Dictionary<ResponsivenessMode, ShiftpointRange>
-        {
-            {ResponsivenessMode.Economic, new ShiftpointRange(new Rpm(1000d), new Rpm(2000d)) }
-        };
-
-        private readonly Dictionary<AggressivenessLevel, Percentage> PercentageForAggressivenesLevel =
-            new Dictionary<AggressivenessLevel, Percentage>
-            {
-                {AggressivenessLevel.First, new Percentage(1.0d) }
-            };
-
-        public RpmBasedByModes(IGearshiftService service)
+        public RpmBasedByModes(IGearshiftService service, Characteristics characteristics)
         {
             _service = service;
+            _characteristics = characteristics;
         }
 
         public void ApplyEvent(IEvent @event)
@@ -33,86 +22,28 @@ namespace GearboxDriver.Processes
             switch (@event)
             {
                 case ComfortModeEntered _:
-                    ResponsivenessMode = ResponsivenessMode.Comfort;
+                    CurrentResponsivenessMode = ResponsivenessMode.Comfort;
                     break;
                 case EconomicModeEntered _:
-                    ResponsivenessMode = ResponsivenessMode.Economic;
+                    CurrentResponsivenessMode = ResponsivenessMode.Economic;
                     break;
                 case SportModeEntered _:
-                    ResponsivenessMode = ResponsivenessMode.Sport;
+                    CurrentResponsivenessMode = ResponsivenessMode.Sport;
                     break;
                 case AggressivenessLevelSelected aggressivenessLevel:
-                    AggressivenessLevel = aggressivenessLevel.Level;
+                    CurrentAggressivenessLevel = aggressivenessLevel.Level;
                     break;
             }
 
             UpdateCurrentMode();
         }
 
-        public void RpmBasedByMode(double modificator = 1)
-        {
-            switch (ResponsivenessMode)
-            {
-                case ResponsivenessMode.Economic:
-                    RpmBasedByEconomicMode(modificator);
-                    break;
-                case ResponsivenessMode.Comfort:
-                    RpmBasedByComfortMode(modificator);
-                    break;
-                case ResponsivenessMode.Sport:
-                    RpmBasedBySportMode(modificator);
-                    break;
-            }
-        }
-
-        public void RpmBasedByAggressivenessLevel(AggressivenessLevel aggressivenessLevel)
-        {
-            AggressivenessLevel = aggressivenessLevel;
-
-            switch (AggressivenessLevel)
-            {
-                case AggressivenessLevel.First:
-                    RpmBasedByMode();
-                    break;
-                case AggressivenessLevel.Second:
-                    RpmBasedByMode(1.3d);
-                    break;
-                case AggressivenessLevel.Third:
-                    RpmBasedByMode(1.3d);
-                    break;
-            }
-        }
-
-        public void RpmBasedByEconomicMode(Percentage percentage)
-        {
-            ResponsivenessMode = ResponsivenessMode.Economic;
-
-            _service.KeepFollowingRpm(RangeForModes[ResponsivenessMode].AsModifiedBy(PercentageForAggressivenesLevel[AggressivenessLevel]));
-        }
-
-        public void RpmBasedByComfortMode(double modificator = 1)
-        {
-            ResponsivenessMode = ResponsivenessMode.Comfort;
-
-            var lowerShiftPoint = new Rpm(1000d * modificator);
-            var upperShiftPoint = new Rpm(2500d * modificator);
-
-            _service.KeepFollowingRpm(new ShiftpointRange(lowerShiftPoint, upperShiftPoint));
-        }
-
-        public void RpmBasedBySportMode(double modificator = 1)
-        {
-            ResponsivenessMode = ResponsivenessMode.Sport;
-
-            var lowerShiftPoint = new Rpm(1500d * modificator);
-            var upperShiftPoint = new Rpm(2500d * modificator);
-
-            _service.KeepFollowingRpm(new ShiftpointRange(lowerShiftPoint, upperShiftPoint));
-        }
-
         private void UpdateCurrentMode()
         {
-            _service.KeepFollowingRpm(RangeForModes[ResponsivenessMode].AsModifiedBy(PercentageForAggressivenesLevel[AggressivenessLevel]));
+            var modeRange = _characteristics.GetRangeForResponsivenessMode(CurrentResponsivenessMode);
+            var aggresivenessPercentage = _characteristics.GetPercentageForAggressivenessLevel(CurrentAggressivenessLevel);
+
+            _service.KeepFollowingRpm(modeRange.AsModifiedBy(aggresivenessPercentage));
         }
     }
 }

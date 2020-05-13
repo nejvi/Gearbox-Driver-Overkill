cb930ec73cbf94debd7fe7cb3f74f4a533a4f7c1
Michał Wityk <michal.wityk@oke.pl>
2020-05-13 21:15:59 +0200

    Adjust tests

--- GearboxDriver.Hardware.ACL/LeverAdapter.cs
+++ GearboxDriver.Hardware.ACL/LeverAdapter.cs
@@ -5,7 +5,7 @@ namespace GearboxDriver.Hardware.ACL
 {
     public class LeverAdapter : ILever
     {
-        private Gearbox _gearbox;
+        private readonly Gearbox _gearbox;
 
         public LeverAdapter(Gearbox gearbox)
         {
@@ -14,26 +14,24 @@ namespace GearboxDriver.Hardware.ACL
 
         public void SetDriveMode()
         {
-            var currentGear = _gearbox.getCurrentGear();
-            if(currentGear == null)
-                currentGear = 1;
-
-            _gearbox.setGearBoxCurrentParams(new Object[2] { 1, currentGear});
+            _gearbox.setGearBoxCurrentParams(new Object[2] { 1, CurrentGearOrFallback });
         }
 
         public void SetNeutralMode()
         {
-            _gearbox.setGearBoxCurrentParams(new Object[2] { 4, 0 });
+            _gearbox.setGearBoxCurrentParams(new Object[2] { 4, CurrentGearOrFallback });
         }
 
         public void SetParkMode()
         {
-            _gearbox.setGearBoxCurrentParams(new Object[2] { 2, 0 });
+            _gearbox.setGearBoxCurrentParams(new Object[2] { 2, CurrentGearOrFallback });
         }
 
         public void SetReverseMode()
         {
-            _gearbox.setGearBoxCurrentParams(new Object[2] { 3, -1 });
+            _gearbox.setGearBoxCurrentParams(new Object[2] { 3, CurrentGearOrFallback });
         }
+
+        private object CurrentGearOrFallback => _gearbox.getCurrentGear() ?? 0;
     }
 }

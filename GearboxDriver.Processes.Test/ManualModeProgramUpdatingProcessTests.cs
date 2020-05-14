9fc492d60615e2132cf50cfb5f8fb1395eaf3db9
Maciej Białobrzeski <bialobrzeskimaciek@gmail.com>
2020-05-14 18:24:26 +0200

    Add ManualModeProgramUpdatingProcess Tests

--- GearboxDriver.Processes.Test/ManualModeProgramUpdatingProcessTests.cs
+++ GearboxDriver.Processes.Test/ManualModeProgramUpdatingProcessTests.cs
@@ -1,10 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GearboxDriver.Gearshift;
+using GearboxDriver.PublishedLanguage.Gearbox;
+using GearboxDriver.PublishedLanguage.ManualGearshifting;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace GearboxDriver.Processes.Test
 {
-    class ManualModeProgramUpdatingProcessTests
+    public class ManualModeProgramUpdatingProcessTests
     {
+        [Test]
+        public void WhenManualGearshiftingModeExitedStopTargetingAnyGear()
+        {
+            var serviceMock = new Mock<IGearshiftService>();
+            var process = new ManualModeProgramUpdatingProcess(serviceMock.Object);
+
+            process.ApplyEvent(new GearChanged(new Gear(2), new Gear(3)));
+            process.ApplyEvent(new ManualGearshiftingModeEntered());
+            process.ApplyEvent(new ManualGearshiftingModeExited());
+
+            serviceMock.Verify(x => x.StopTargetingAnyGear(), Times.Once);
+        }
+
+        [Test]
+        public void WhenManualGearshiftingModeEnteredStartTargetingGear()
+        {
+            var serviceMock = new Mock<IGearshiftService>();
+            var process = new ManualModeProgramUpdatingProcess(serviceMock.Object);
+
+            process.ApplyEvent(new GearChanged(new Gear(2), new Gear(3)));
+            process.ApplyEvent(new ManualGearshiftingModeEntered());
+
+            serviceMock.Verify(x => x.TargetGear(new Gear(2)), Times.Once);
+        }
+
+        [Test]
+        public void WhenGearUpshiftedManuallyGearIsUpshifted()
+        {
+            var serviceMock = new Mock<IGearshiftService>();
+            var process = new ManualModeProgramUpdatingProcess(serviceMock.Object);
+
+            process.ApplyEvent(new GearChanged(new Gear(2), new Gear(3)));
+            process.ApplyEvent(new ManualGearshiftingModeEntered());
+            process.ApplyEvent(new GearUpshiftedManually());
+
+            serviceMock.Verify(x => x.TargetGear(new Gear(3)));
+        }
+
+        [Test]
+        public void WhenGearDownshiftedManuallyGearIsDownshifted()
+        {
+            var serviceMock = new Mock<IGearshiftService>();
+            var process = new ManualModeProgramUpdatingProcess(serviceMock.Object);
+
+            process.ApplyEvent(new GearChanged(new Gear(2), new Gear(3)));
+            process.ApplyEvent(new ManualGearshiftingModeEntered());
+            process.ApplyEvent(new GearUpshiftedManually());
+
+            serviceMock.Verify(x => x.TargetGear(new Gear(2)));
+        }
     }
 }

1fe8f37f8a69e65f9439c5edfae4d2d6a529a7fd
Michał Wityk <michal.wityk@oke.pl>
2020-05-14 23:20:00 +0200

    Update sample program

--- GearboxDriver.SampleApplication/Program.cs
+++ GearboxDriver.SampleApplication/Program.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Threading.Tasks;
-using GearboxDriver.CabinControls;
-using GearboxDriver.Gearshift;
-using GearboxDriver.Gearshift.Negotiation;
-using GearboxDriver.Gearshift.Shifting;
-using GearboxDriver.Hardware.ACL;
-using GearboxDriver.Hardware.API;
-using GearboxDriver.Processes;
-using GearboxDriver.PublishedLanguage.Pedals;
-using GearboxDriver.PublishedLanguage.Responsiveness;
 using GearboxDriver.SampleApplication.Demo;
-using GearboxDriver.Seedwork;
 
 namespace GearboxDriver.SampleApplication
 {
@@ -18,19 +7,10 @@ namespace GearboxDriver.SampleApplication
     {
         static void Main(string[] args)
         {
-           
-
             DemoShow.Play();
-
-            
 
             Console.ReadKey();
         }
 
-        public void Setup()
-        {
-            
-
-        }
     }
 }
